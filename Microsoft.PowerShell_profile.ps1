af8e5edad5c55d083af1889c40962e5f2caa500e
Mozzie <donytiara@gmail.com>
2025-09-12 17:26:22 +0700

    refactor

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,11 @@
 # Load opener functions
-. "$PSScriptRoot\opwsh\opwsh.ps1"
+. "$PSScriptRoot\everything_powershell\opwsh.ps1"
 
 # Load Everything SDK wrapper
-. "$PSScriptRoot\epwsh\epwsh.ps1"
+. "$PSScriptRoot\everything_powershell\epwsh.ps1"
 
 # Load ImageTools functions
-. "$PSScriptRoot\imageto\convert-images.ps1"
+. "$PSScriptRoot\everything_powershell\convert-images.ps1"
 
 # Auto cd to the foreground Windows Explorer folder
-. "$PSScriptRoot\utils\auto_cd_quake_mode.ps1"
+. "$PSScriptRoot\everything_powershell\auto_cd_quake_mode.ps1"
