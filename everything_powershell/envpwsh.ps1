8ee7a73f14d0d08897737a667cde1c6455d9583c
Mozzie <donytiara@gmail.com>
2025-09-13 14:02:40 +0700

    boom

--- everything_powershell/envpwsh.ps1
+++ everything_powershell/envpwsh.ps1
@@ -1,22 +1,13 @@
 # Retrieve the current user's Path environment variable
 
-function Get-EnvironmentVariables {
+function envs {
+    param(
+        [string]$Scope,
+        [string]$Var,
+        # TODO: add value param for SetEnvironmentVariable
+        [string]$Value
+    )
+
     # Get the Path environment variable for the current user
-    $path = [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-    # Check if the Path variable exists
-    if ($null -eq $path) {
-        Write-Output "No Path variable found for the current user."
-    } else {
-        # Split the Path into individual directories (using platform-appropriate delimiter)
-        $delimiter = if ($env:OS -like "Windows*") { ";" } else { ":" }
-        $pathArray = $path -split $delimiter
-
-        # Display in a readable format with numbering
-        Write-Output "Current User's Path Environment Variable:"
-        Write-Output "----------------------------------------"
-        $pathArray | ForEach-Object { $i = 1 } { Write-Output "$i. $_"; $i++ }
-    }
+    [Environment]::GetEnvironmentVariable($Var, $Scope)
 }
-
-Set-Alias -Name pow -Value Get-EnvironmentVariables
